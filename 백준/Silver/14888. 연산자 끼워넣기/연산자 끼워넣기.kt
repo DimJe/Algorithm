56fde47748cc56000660da65c7820c9eedb10772
DimJe <mark759@naver.com>
2024-05-07 22:21:53 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 196 ms, Memory: 22320 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.kt
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.kt
@@ -13,6 +13,8 @@ fun main() = with(System.`in`.bufferedReader()){
     cal = readln().split(" ").map { it.toInt() }.toMutableList()
 
     dfs(1,arr[0])
+    //dfsMax(1,arr[0])
+    //dfsMin(1,arr[0])
 
     println("$max\n$min")
 }
@@ -21,6 +23,51 @@ fun dfs(index: Int,sum: Int){
     if(index == n){
         min = min(min,sum)
         max = max(max,sum)
+        return
+    }
+    for (i in 0..3){
+        if(cal[i] != 0){
+            cal[i]--
+            when(i){
+                0 -> dfs(index + 1,sum + arr[index])
+                1 -> dfs(index + 1,sum - arr[index])
+                2 -> dfs(index + 1,sum * arr[index])
+                3 -> dfs(index + 1,sum / arr[index])
+            }
+            cal[i]++
+        }
+    }
+}
+fun dfsMax(index: Int,sum: Int){
+
+    if(index == n){
+        max = max(max,sum)
+        return
+    }
+    if(cal[0] == 0 && cal[2]==0 && index < n){
+        return
+    }
+    for (i in 0..3){
+        if(cal[i] != 0){
+            cal[i]--
+            when(i){
+                0 -> dfs(index + 1,sum + arr[index])
+                1 -> dfs(index + 1,sum - arr[index])
+                2 -> dfs(index + 1,sum * arr[index])
+                3 -> dfs(index + 1,sum / arr[index])
+            }
+            cal[i]++
+        }
+    }
+}
+fun dfsMin(index: Int,sum: Int){
+
+    if(index == n){
+        min = min(min,sum)
+        return
+    }
+    if(cal[1] == 0 && cal[3]==0 && index < n){
+        return
     }
     for (i in 0..3){
         if(cal[i] != 0){
